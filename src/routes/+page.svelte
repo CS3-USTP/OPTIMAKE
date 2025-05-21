9eccb1e5657b2d9f4d816d71044ad7bc245bb7a7
mihkuno <caindayjoeninyo@gmail.com>
2025-05-21 19:14:29 +0800

    added sidebar trigger and breadcrumbs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,6 +18,8 @@
 	import { slide } from "svelte/transition";
 	// shadcn/ui
 	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
+	import { Separator } from "$lib/components/ui/separator/index.js";
 	// custom components
 	import NavWorkspace from "$lib/components/nav-workspace.svelte";
 	import NavUser from "$lib/components/nav-user.svelte";
@@ -204,4 +206,32 @@
 
 		<Sidebar.Rail />
 	</Sidebar.Root>
+	<Sidebar.Inset>
+		<header
+			class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+			<div class="flex items-center gap-2 px-4">
+				<Sidebar.Trigger class="-ml-1" />
+				<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+				<Breadcrumb.Root>
+					<Breadcrumb.List>
+						<Breadcrumb.Item class="hidden md:block">
+							<Breadcrumb.Link href="#">Building Your Application</Breadcrumb.Link>
+						</Breadcrumb.Item>
+						<Breadcrumb.Separator class="hidden md:block" />
+						<Breadcrumb.Item>
+							<Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
+						</Breadcrumb.Item>
+					</Breadcrumb.List>
+				</Breadcrumb.Root>
+			</div>
+		</header>
+		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+			<div class="grid auto-rows-min gap-4 md:grid-cols-3">
+				<div class="bg-muted/50 aspect-video rounded-xl"></div>
+				<div class="bg-muted/50 aspect-video rounded-xl"></div>
+				<div class="bg-muted/50 aspect-video rounded-xl"></div>
+			</div>
+			<div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min"></div>
+		</div>
+	</Sidebar.Inset>
 </Sidebar.Provider>
